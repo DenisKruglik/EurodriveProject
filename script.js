8eacd12f67675bacb10f25b7fa8bf1d9d4c0e517
DenisKruglik <d.kruglik11011@gmail.com>
2016-12-24 21:50:20 +0300

    Update JavaScript

--- script.js
+++ script.js
@@ -1,48 +1,77 @@
-location.hash="#main";
-function serve(){
-	var h=location.hash;
-	var path="";
-	switch(h){
-		case "#main":
-			path="main.html";
-			break;
-		case "#broning":
-			path="broning.html";
-			break;
-		case "#contacs":
-			path="contacs.html";
-			break;
-		case "#faq":
-			path="faq.html";
-			break;
-		case "#stops":
-			path="stops.html";
-			break;	
-		default: return;		
+var App={
+	init:function(){
+		App.setFormEvents();
+		App.setLocalStorage();
+		App.syncLocalStorage();
+	},
+	setFormEvents:function(){
+		$('#form').submit(function(e){
+			e.preventDefault();
+			if ($('input[type=email]').val()) {
+				App.dropMessage('Ваша заявка успешно отправлена. На Ваш номер и электронный адрес выслано уведомление',$('#form'));
+			}else App.dropMessage('Ваша заявка успешно отправлена. На Ваш номер выслано уведомление',$('#form'));
+		})
+		$('select[name=direction]').change(function(){
+			App.switchSelect(this);
+		})
+	},
+	switchSelect:function(e){
+		if (e.value=="Гомель-Минск") {
+			$('option.fromminsk').css('display','none');
+			$('option.fromgomel').css('display','block');
+			$('option.togomel').css('display','none');
+			$('option.tominsk').css('display','block');
+		}else if (e.value=="Минск-Гомель"){
+			$('option.fromminsk').css('display','block');
+			$('option.fromgomel').css('display','none');
+			$('option.togomel').css('display','block');
+			$('option.tominsk').css('display','none');
+		}else $('select[name=onset] option, select[name=destination] option').css('display','none');
+	},
+	dropMessage:function(e,loc){
+		var mes=$("<div class='message'>"+e+"</div>");
+		$(mes).css('display','none');
+		$(mes).css('position','fixed');
+		$(mes).css('top','60px');
+		$(mes).prependTo(loc);
+		$(mes).fadeIn(500);
+		var timer=setTimeout(function(){
+			$(mes).fadeOut(500);
+		},5000);
+		var rem=setTimeout(function(){
+			$(mes).remove();
+		},5500);
+	},
+	setLocalStorage:function(){
+		$('#form input, #form textarea').on('input',function(){
+			App.remember(this);
+		})
+		$('#form select').on('change',function(){
+			App.remember(this);
+		})
+		$('#form').on('reset',function(e){
+			e.preventDefault();
+			localStorage.clear();
+			for (var i = 0; i < form.elements.length-2; i++) {
+				form.elements[i].value="";
+			}
+			App.switchSelect($('select[name=direction]')[0]);
+		})
+	},
+	remember:function(e){
+		var a=$(e).attr('name');
+		localStorage.setItem(a,e.value);
+	},
+	syncLocalStorage:function(){
+		$(document).ready(function(){
+			var form=document.getElementById('form');
+			for (var i = 0; i < form.elements.length-2; i++) {
+				var n=form.elements[i].name;
+				form.elements[i].value=localStorage.getItem(n);
+			}
+			var destination=$('select[name=direction]')[0];
+			App.switchSelect(destination);
+		})
 	}
-	var xhr=new XMLHttpRequest();
-	xhr.onreadystatechange=function(){
-		if(this.readyState==4 && this.status==200){
-			document.getElementById('container').innerHTML=this.responseText;
-			if (path=="main.html") {fr.event("click",get.byId('b'),function(){location.hash="#broning"})}
-			if (path=="broning.html") {fr.event("click",get.byId('notif'),function(){
-				if (get.byId('tel').value.length<7 || !Number(get.byId('tel').value)) {
-					alert("Введите корректный телефонный номер в соответствующее поле");
-				}else alert("Оповещение будет отправлено на Ваш номер");
-		})}
-			if (path=="broning.html") {fr.event("click",get.byId('rate'),function(){
-				for (;;) {
-					var a=prompt("Введите Вашу оценку от 1 до 5");
-					if (a==1 || a==2 || a==3 || a==4 || a==5){
-						alert("Ваша оценка учтена в общем рейтинге");
-						break;
-					}else alert("Неверный ввод");
-				}
-				})}
-		}
-	}
-	xhr.open("GET",path,true);
-	xhr.send();
 }
-window.onhashchange=serve;
-serve();
+App.init();
